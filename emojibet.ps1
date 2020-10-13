ed65ea50fee1d4b9ebec71372c4469b31dd15e0e
Eugene <eugene@gghq.io>
2020-10-13 10:27:47 +0100

    Added whitespace and numbers.

--- emojibet.ps1
+++ emojibet.ps1
@@ -28,6 +28,17 @@
     x {':regional_indicator_x:'}
     y {':regional_indicator_y:'}
     z {':regional_indicator_z:'}
+    ' ' {':small_orange_diamond:'}
+    1 {':one:'}
+    2 {':two:'}
+    3 {':three:'}
+    4 {':four:'}
+    5 {':five:'}
+    6 {':six:'}
+    7 {':seven:'}
+    8 {':eight:'}
+    9 {':nine:'}
+    0 {':zero:'}
     default {$_}
   }
 )
